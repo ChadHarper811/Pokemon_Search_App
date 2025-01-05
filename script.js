f60165bdcc79c363933edfb16f8ae2b82895c989
Chad Harper <chadharper811@gmail.com>
2025-01-04 19:52:19 -0600

    added delay timer so start up animation can finish before event listeners are active

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ const startingText = document.getElementById("starting-text");
 const startBtn = document.getElementById("start-button");
 
 
+let startingDelayFinsihed = false;
+
+const delayFinished = () => {
+    startingDelayFinsihed = true
+}
+
+const startingDelay = setTimeout(delayFinished, 4500);
+
+
+const startUp = () => {
+    startingText.style.display = "none";
+}
 
 startBtn.addEventListener("click", () => {
-    startingText.style.display = "none";
+    if(startingDelayFinsihed === true) {
+        startUp()
+    }
 })
 
 document.addEventListener("keypress", (e) => {
-    if(e.key === "Enter") {
-        startingText.style.display = "none";
+    if(e.key === "Enter" && startingDelayFinsihed === true) {
+        startUp()
     }
 })
