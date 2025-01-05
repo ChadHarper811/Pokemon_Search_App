38cddd1c55b488cdad2a6a43fba10d7985643908
Chad Harper <chadharper811@gmail.com>
2025-01-04 20:19:56 -0600

    chnages to script, removed light blink effect

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// variables
 const startingText = document.getElementById("starting-text");
 const startBtn = document.getElementById("start-button");
+const batteryLight = document.getElementById("battery-light");
+const batteryLabel = document.getElementById("battery-label")
 
-
+// delaying event handlers so start up animation can finish
 let startingDelayFinsihed = false;
 
 const delayFinished = () => {
@@ -10,11 +13,12 @@ const delayFinished = () => {
 
 const startingDelay = setTimeout(delayFinished, 4500);
 
-
+// functions
 const startUp = () => {
     startingText.style.display = "none";
 }
 
+// event handlers
 startBtn.addEventListener("click", () => {
     if(startingDelayFinsihed === true) {
         startUp()
